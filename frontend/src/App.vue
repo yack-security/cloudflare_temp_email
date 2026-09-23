f347988d7925862a81233805d512a3b7e195da2e
someyack <nicholas@yack.one>
2026-09-23 14:48:12 -0400

    up

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,129 +1,116 @@
 <script setup>
-import { darkTheme, NGlobalStyle, zhCN, enUS } from 'naive-ui'
-import { computed, onMounted } from 'vue'
-import { useI18n } from 'vue-i18n'
-import { useGlobalState } from './store'
-import { useIsMobile } from './utils/composables'
-import Header from './views/Header.vue';
-import Footer from './views/Footer.vue';
-import { api } from './api'
+import { darkTheme, NGlobalStyle, zhCN, enUS } from "naive-ui";
+import { computed, onMounted } from "vue";
+import { useI18n } from "vue-i18n";
+import { useGlobalState } from "./store";
+import { useIsMobile } from "./utils/composables";
+import Header from "./views/Header.vue";
+import Footer from "./views/Footer.vue";
+import { api } from "./api";
 
-const {
-  isDark, loading, useSideMargin, telegramApp, isTelegram
-} = useGlobalState()
+const { isDark, loading, useSideMargin, telegramApp, isTelegram } =
+    useGlobalState();
 const { locale } = useI18n({});
-const theme = computed(() => isDark.value ? darkTheme : null)
+const theme = computed(() => (isDark.value ? darkTheme : null));
 // const localeConfig = computed(() => locale.value == 'zh' ? zhCN : null)
-const localeConfig = computed(() => locale.value == 'en' ? enUS : null)
-const isMobile = useIsMobile()
+const localeConfig = computed(() => (locale.value == "en" ? enUS : null));
+const isMobile = useIsMobile();
 const showSideMargin = computed(() => !isMobile.value && useSideMargin.value);
 
-
 onMounted(async () => {
+    try {
+        await api.getUserSettings();
+    } catch (error) {
+        console.error(error);
+    }
 
-  try {
-    await api.getUserSettings();
-  } catch (error) {
-    console.error(error);
-  }
+    const token = import.meta.env.VITE_CF_WEB_ANALY_TOKEN;
 
-  const token = import.meta.env.VITE_CF_WEB_ANALY_TOKEN;
+    const exist =
+        document.querySelector(
+            'script[src="https://static.cloudflareinsights.com/beacon.min.js"]',
+        ) !== null;
+    if (token && !exist) {
+        const script = document.createElement("script");
+        script.defer = true;
+        script.src = "https://static.cloudflareinsights.com/beacon.min.js";
+        script.dataset.cfBeacon = JSON.stringify({ token: token });
+        document.body.appendChild(script);
+    }
 
-  const exist = document.querySelector('script[src="https://static.cloudflareinsights.com/beacon.min.js"]') !== null
-  if (token && !exist) {
-    const script = document.createElement('script');
-    script.defer = true;
-    script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
-    script.dataset.cfBeacon = JSON.stringify({ token: token });
-    document.body.appendChild(script);
-  }
-
-  // Add Counterscale tracking
-  const counterscaleExists = document.querySelector('#counterscale-script') !== null;
-  if (!counterscaleExists) {
-    const script = document.createElement('script');
-    script.id = 'counterscale-script';
-    script.defer = true;
-    script.dataset.siteId = 'temp-mail.yack.one';
-    script.src = 'https://track.yack.one/tracker.js';
-    document.body.appendChild(script);
-  }
-
-  // check if telegram is enabled
-  const enableTelegram = import.meta.env.VITE_IS_TELEGRAM;
-  if (
-    (typeof enableTelegram === 'boolean' && enableTelegram === true)
-    ||
-    (typeof enableTelegram === 'string' && enableTelegram === 'true')
-  ) {
-    await new Promise((resolve, reject) => {
-      const script = document.createElement('script');
-      script.src = 'https://telegram.org/js/telegram-web-app.js';
-      script.onload = resolve;
-      script.onerror = reject;
-      document.body.appendChild(script);
-    });
-    telegramApp.value = window.Telegram?.WebApp || {};
-    isTelegram.value = !!window.Telegram?.WebApp?.initData;
-  }
+    // check if telegram is enabled
+    const enableTelegram = import.meta.env.VITE_IS_TELEGRAM;
+    if (
+        (typeof enableTelegram === "boolean" && enableTelegram === true) ||
+        (typeof enableTelegram === "string" && enableTelegram === "true")
+    ) {
+        await new Promise((resolve, reject) => {
+            const script = document.createElement("script");
+            script.src = "https://telegram.org/js/telegram-web-app.js";
+            script.onload = resolve;
+            script.onerror = reject;
+            document.body.appendChild(script);
+        });
+        telegramApp.value = window.Telegram?.WebApp || {};
+        isTelegram.value = !!window.Telegram?.WebApp?.initData;
+    }
 });
 </script>
 
 <template>
-  <n-config-provider :locale="localeConfig" :theme="theme">
-    <n-global-style />
-    <n-spin description="loading..." :show="loading">
-      <n-notification-provider container-style="margin-top: 60px;">
-        <n-message-provider container-style="margin-top: 20px;">
-          <n-grid x-gap="12" :cols="12">
-            <n-gi v-if="showSideMargin" span="1"></n-gi>
-            <n-gi :span="!showSideMargin ? 12 : 10">
-              <div class="main">
-                <n-space vertical>
-                  <n-layout style="min-height: 80vh;">
-                    <Header />
-                    <router-view></router-view>
-                  </n-layout>
-                  <Footer />
-                </n-space>
-              </div>
-            </n-gi>
-            <n-gi v-if="showSideMargin" span="1"></n-gi>
-          </n-grid>
-          <n-back-top />
-        </n-message-provider>
-      </n-notification-provider>
-    </n-spin>
-  </n-config-provider>
+    <n-config-provider :locale="localeConfig" :theme="theme">
+        <n-global-style />
+        <n-spin description="loading..." :show="loading">
+            <n-notification-provider container-style="margin-top: 60px;">
+                <n-message-provider container-style="margin-top: 20px;">
+                    <n-grid x-gap="12" :cols="12">
+                        <n-gi v-if="showSideMargin" span="1"></n-gi>
+                        <n-gi :span="!showSideMargin ? 12 : 10">
+                            <div class="main">
+                                <n-space vertical>
+                                    <n-layout style="min-height: 80vh">
+                                        <Header />
+                                        <router-view></router-view>
+                                    </n-layout>
+                                    <Footer />
+                                </n-space>
+                            </div>
+                        </n-gi>
+                        <n-gi v-if="showSideMargin" span="1"></n-gi>
+                    </n-grid>
+                    <n-back-top />
+                </n-message-provider>
+            </n-notification-provider>
+        </n-spin>
+    </n-config-provider>
 </template>
-
 
 <style>
 .n-switch {
-  margin-left: 10px;
-  margin-right: 10px;
+    margin-left: 10px;
+    margin-right: 10px;
 }
 </style>
 
 <style scoped>
 .side {
-  height: 100vh;
+    height: 100vh;
 }
 
 .main {
-  height: 100vh;
-  text-align: center;
+    height: 100vh;
+    text-align: center;
 }
 
 .n-grid {
-  height: 100%;
+    height: 100%;
 }
 
 .n-gi {
-  height: 100%;
+    height: 100%;
 }
 
 .n-space {
-  height: 100%;
+    height: 100%;
 }
 </style>
